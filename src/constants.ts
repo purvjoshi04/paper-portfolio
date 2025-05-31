2eab7f6bcc74cb8ea37743a68f4ff7ade6989e96
Purv Joshi <purvadjoshi@gmail.com>
2025-05-31 15:24:24 +0530

    changed paper theme, header and footer content and social links

--- src/constants.ts
+++ src/constants.ts
@@ -11,25 +11,29 @@ import { SITE } from "@/config";
 export const SOCIALS = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/purvjoshi04",
+    EventTarget: "_blank",
     linkTitle: ` ${SITE.title} on Github`,
     icon: IconGitHub,
   },
   {
     name: "X",
-    href: "https://x.com/username",
+    href: "https://x.com/purv_joshi_",
+    EventTarget: "_blank",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/in/purvjoshi/",
+    EventTarget: "_blank",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:purvjoshi.dev@gmail.com",
+    EventTarget: "_blank",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
